fa53fa724055c78d73f78740be04740c8e2ddf88
L42Matheus <77408554+L42Matheus@users.noreply.github.com>
2022-04-07 14:57:22 -0300

    Melhorias taDentro.

--- src/main/java/br/com/zup/TaDentro/TaDentroApplication.java
+++ src/main/java/br/com/zup/TaDentro/TaDentroApplication.java
@@ -1,7 +1,10 @@
 package br.com.zup.TaDentro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class TaDentroApplication {
